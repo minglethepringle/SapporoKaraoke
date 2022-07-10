81ddd403fc0ce958a76ad4e1ebb22977f7e66c9d
Mingle Li <limingle5@gmail.com>
2022-07-10 16:08:20 -0400

    Added firebase, cannot choose songs out of karaoke hours

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ import Footer from "./components/footer/Footer";
 import EnterPage from './components/pages/enter/EnterPage';
 import SearchPage from './components/pages/search/SearchPage';
 import { Slide, ToastContainer } from "react-toastify";
+import firebaseApp from "./shared/FirebaseAPI";
+import { get, getDatabase, ref } from "firebase/database";
+import makeToast from "./shared/MakeToast";
+import moment from "moment";
 
 function App() {
+  let [karaokePrefs, setKaraokePrefs] = useState({});
+  let [karaokeTime, setKaraokeTime] = useState(false);
   let [showSearch, setShowSearch] = useState(false);
   let localStorageKey = "following2";
 
@@ -36,6 +42,35 @@ function App() {
   }
 
   /**
+   * Effect: get karaoke preferences and check times. Runs once
+   */
+  useEffect(() => {
+    // Weird bug where this useEffect called multiple times. If prefs non-empty, don't call db again.
+    if (Object.keys(karaokePrefs).length > 0) {
+      return;
+    }
+
+    // Initialize Realtime Database and get a reference to the service
+    const db = getDatabase(firebaseApp);
+    get(ref(db)).then((snapshot) => {
+      if (snapshot.exists()) {
+        let data = snapshot.val();
+        setKaraokePrefs(data);
+      } else {
+        return makeToast("RTDB Error!", "error");
+      }
+    }).catch((error) => {
+      console.log(error);
+      return makeToast("RTDB Error!", "error");
+    });
+  }, []);
+
+  /**
+   * Effect: Check karaoke times based on karaokePrefs update. Setter is async!
+   */
+  useEffect(checkKaraokeTimes, [karaokePrefs]);
+
+  /**
    * Effect: Checks local storage to see if user is following
    */
   useEffect(() => {
@@ -44,15 +79,39 @@ function App() {
     }
   });
 
+  /**
+   * Checks if right now is during karaoke hours. 
+   */
+  function checkKaraokeTimes() {
+    if (Object.keys(karaokePrefs).length === 0) {
+      return;
+    }
+
+    let now = moment();
+    let startTime = moment(karaokePrefs.karaokeStart, "HH:mm");
+    let endTime = moment(karaokePrefs.karaokeEnd, "HH:mm");
+
+    // false if out of time, true if within time
+    setKaraokeTime(!(now.isBefore(startTime) || now.isAfter(endTime)));
+  }
+
   return (
     <>
       <div className="App">
         <Header />
 
         {
-          showSearch ?
-            <SearchPage /> :
-            <EnterPage setShowSearch={setShowSearch} setFollowing={setFollowing} />
+          (!karaokeTime) ?
+            <main className="text-center">
+              <h1>It is currently not during karaoke hours.</h1>
+              <h3>Karaoke hours are from {karaokePrefs.karaokeStart} to {karaokePrefs.karaokeEnd}.</h3>
+              <br/>
+              <h2>See you soon!</h2>
+            </main>
+            :
+            (showSearch) ?
+              <SearchPage /> :
+              <EnterPage setShowSearch={setShowSearch} setFollowing={setFollowing} />
         }
 
         <Footer />
